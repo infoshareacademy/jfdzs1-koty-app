1fecadb4c7f7e6f2df1e8624b3659577419e15bb
zosia <zofia.subocz@gmail.com>
2018-03-11 13:11:47 +0100

    Init app

---
+++ src/components/dashboard/Dashboard.component.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+
+class Dashboard extends PureComponent {
+    render() {
+        return (
+            <div>Dashboard</div>
+
+        );
+    }
+}
+
+export default Dashboard;
---
+++ src/components/menu/Menu.component.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+
+class Menu extends PureComponent {
+    render() {
+        return (
+            <div>Dashboard</div>
+
+        );
+    }
+}
+
+export default Menu;
