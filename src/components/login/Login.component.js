9742d38e809e85318a85f3f5503c4506feb0c6f1
KasiaTorz <ktorz@wp.pl>
2018-03-12 15:04:15 +0100

    add state

--- src/components/login/Login.component.js
+++ src/components/login/Login.component.js
@@ -3,11 +3,18 @@ import './Login.style.css';
 
 
 class Login extends PureComponent {
+    state = {
+        firstName: '',
+        lastName: '',
+        userName:'',
+        password:'',
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    
+                    <input placeholder='First Name' value={this.state.firstName}/>
                 </form>
             </div>
 
