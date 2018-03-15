9a9667c77d2db8ba9ea8b684fabe09c41d134ff8
KasiaTorz <ktorz@wp.pl>
2018-03-15 12:43:35 +0100

    add form in function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form onSubmit={fields => } />
+        <Form onSubmit={fields => this.onSubmit } />
       </div>
     );
   }
