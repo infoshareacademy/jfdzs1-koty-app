0e85da101247d0348dd897c1dc06cde633eaa65c
Kate Torz <ktorz@wp.pl>
2018-03-18 12:25:41 +0100

    corect form

--- src/components/login/Login.component.js
+++ src/components/login/Login.component.js
@@ -1,36 +1,25 @@
 import React, {PureComponent} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import './Login.style.css';
 
-export default class Login extends React.PureComponent {
+class Login extends PureComponent {
     state = {
-        firstName: '',
-        lastName: '',
-        userName:'',
+        email:'',
         password:'',
     };
 
     change = e => {
-    this.props.onchange({[e.target.name]: e.target.value});
-    this.setState({
-    [e.target.name]: e.target.value
-      });
+            this.setState({
+                [e.target.name]: e.target.value
+            });
     };
 
     onSubmit = e => {
         e.preventDefault();
 
         this.setState({
-            firstName: "",
-            lastName: "",
-            username: "",
-            email: "",
-            password: ""
-        });
-        this.props.onChange({
-            firstName: "",
-            lastName: "",
-            username: "",
             email: "",
             password: ""
         });
@@ -39,54 +28,37 @@ export default class Login extends React.PureComponent {
 
     render() {
         return (
-                <form>
-                    <TextField
-                        name= "firstName"
-                        hintText="First Name"
-                        floatingLabelText="First name"
-                        value = {this.state.firstName}
-                        onChange={e => this.change(e)}
-                        floatingLabelFixed
-                    />
+            <MuiThemeProvider>
+                <form className="login-form">
+                    <div>
+                        <TextField
+                            name="email"
+                            hintText="email"
+                            floatingLabelText="email"
+                            value={this.state.email}
+                            onChange={e => this.change(e)}
+                            floatingLabelFixed
+                        />
+                    </div>
 
-                <br />
-                    <TextField
-                        name= "lastName"
-                        hintText="Last Name"
-                        floatingLabelText="Last name"
-                        value = {this.state.lastName}
-                        onChange={e => this.change(e)}
-                        floatingLabelFixed
-                    />
-                <br />
-                    <TextField
-                        name= "username"
-                        hintText="Username"
-                        floatingLabelText="Username"
-                        value = {this.state.userName}
-                        onChange={e => this.change(e)}
-                        floatingLabelFixed
-                    />
-                <br />
-                    <TextField
-                        name= "email"
-                        hintText="email"
-                        floatingLabelText="email"
-                        value = {this.state.email}
-                        onChange={e => this.change(e)}
-                        floatingLabelFixed
-                    />
-                    <TextField
-                        name= "password"
+                    <div>
+                        <TextField
+                        name="password"
                         hintText="password"
                         floatingLabelText="password"
-                        value = {this.state.password}
+                        value={this.state.password}
                         onChange={e => this.change(e)}
                         floatingLabelFixed
                     />
-                    <br />
-                    <RaisedButton label="Submit" onClick={e => this.onSubmit(e)}secondary />
+                    </div>
+
+                    <div>
+                        <RaisedButton label="Submit" onClick={e => this.onSubmit(e)}secondary />
+                    </div>
+
                 </form>
-        );
+            </MuiThemeProvider>
+                );
     }
 }
+export default Login
