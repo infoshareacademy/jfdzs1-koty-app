c623828ffec5d6b2307639b5ceab53e4fc0f7c81
KasiaTorz <ktorz@wp.pl>
2018-03-20 18:41:07 +0100

    create scheme to code

---
+++ src/components/form/Form.component.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+import './Form.style.css';
+
+class Form extends PureComponent {
+
+
+    render() {
+        return (
+
+        );
+    }
+}
+export default Form
