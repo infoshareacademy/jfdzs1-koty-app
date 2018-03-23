cde926f396b8f3847d877372bccf5316edd13eb1
KasiaTorz <ktorz@wp.pl>
2018-03-23 20:51:28 +0100

    create constructor

--- src/components/form/Form.component.js
+++ src/components/form/Form.component.js
@@ -2,13 +2,18 @@ import React, {PureComponent} from 'react';
 import './Form.style.css';
 
 class Form extends PureComponent {
+    constructor(){
+        super()
+    }
+
+
  render (){
      return(
          <div>
              <ul>
 
              </ul>
-             <input type="text" defaultValue=""/>
+             <input type="text" defaultValue={this.state.search}/>
          </div>
      )
  }
