d3a5a51b08e14a7f5e2eee2ba4b3b19d82b7baac
mszydelko <m_szydelko@yahoo.pl>
2018-03-31 15:26:17 +0200

    add BarRechart component

--- src/components/dashboardHome/BarRechart.js
+++ src/components/dashboardHome/BarRechart.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
-const LineRechart = () => {
+const BarRechart = () => {
     const data = [
         { name: 'a', value: 12,  pv: 2400, uv: 200, xv: 500 },
         { name: 'b', value: 52, pv: 1398, uv: 300, xv: 1000 },
@@ -10,19 +10,17 @@ const LineRechart = () => {
 
     return(
         <ResponsiveContainer width="90%" height="100%">
-            <LineChart data={data}
-                       margin={{ top: 40, right: 30, left: 20, bottom: 5 }}>
+            <BarChart data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="pv" stroke="red" />
-                <Line type="monotone" dataKey="uv" stroke="blue" />
-                <Line type="monotone" dataKey="xv" stroke="green" />
-            </LineChart>
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
         </ResponsiveContainer>
     )
-}
+};
 
-export default LineRechart;
+export default BarRechart;
--- src/components/dashboardHome/DashboardHome.component.js
+++ src/components/dashboardHome/DashboardHome.component.js
@@ -2,7 +2,8 @@ import React, {PureComponent} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './DashboardHome.style.css';
 import LineRechart from './LineRechart';
-import RadialRechart from './RadialRechart'
+import RadialRechart from './RadialRechart';
+import BarRechart from './BarRechart';
 
 
 
@@ -11,18 +12,18 @@ class DashboardHome extends PureComponent {
     render() {
         return (
             <Grid middle="xs" fluid>
-                <Row className="home-row" center="xs" around="xs">
+                <Row className="home-row" center="xs" middle="xs" around="xs">
                     <Col className="home-items" md={5}>
                         <LineRechart/>
                     </Col>
                     <Col className="home-items" md={4}>
-                        <LineRechart/>
+                        <BarRechart/>
                     </Col>
                     <Col className="home-items" md={3}>
                         <RadialRechart/>
                     </Col>
                 </Row>
-                <Row className="home-row" center="xs" around="xs">
+                <Row className="home-row" center="xs" middle="xs" around="xs">
                     <Col className="home-items" md={3}>
                         <h3>Pliki do pobrania</h3>
                         <i className="material-icons">file_download</i>
